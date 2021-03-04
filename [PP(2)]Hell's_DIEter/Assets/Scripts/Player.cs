1ac3a587f7862e85a41bc2e9f7c759b34a52e788
Haebny <bean9194@naver.com>
2021-03-04 23:33:58 +0900

    제트팩 사용 시 파티클 추가
    - 캐릭터 점프 중 스페이스바가 눌려있으면 작동하는 제트팩에 파티클을 추가
    - UI(체력, 연료, 몸무게, 코인, 아이템 리스트, 미니맵, 메뉴, 설정 등) 추가 필요
    - 사이즈 시스템 추가 필요(주인공 몸무게에 따라 들어갈 수 있는 문 크기 변동)

--- [PP(2)]Hell's_DIEter/Assets/Scripts/Player.cs
+++ [PP(2)]Hell's_DIEter/Assets/Scripts/Player.cs
@@ -4,111 +4,136 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	private Animator anim;
-    private CharacterController controller; 
+    private Camera cam;
+    private Rigidbody rb;
+    private ParticleSystem particle;
 
-    public float speed = 600.0f;
-	public float turnSpeed = 400.0f;
-	private Vector3 moveDirection = Vector3.zero;
-
-    public bool isJetpackOn;       // 제트팩 사용 확인
-
-    private float verticalVelocity;
-    public float gravity = 14.0f;
-    public float jumpForce = 3.0f;     // 점프 힘
+    [SerializeField] private bool isJetpackOn;       // 제트팩 사용 확인
+    [SerializeField] private float fuel = 10.0f;      // 비행 연료
+    [SerializeField] private bool isGrounded;
+    private float gravity = 14.0f;
+    private float speed = 5.0f;
+    private float jumpForce = 10.0f;     // 점프 힘
     private float maximumFuel;
-    public float fuel = 3.0f;      // 비행 연료
 
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
 
     void Start () {
 		anim = gameObject.GetComponentInChildren<Animator>();
-        controller = GetComponent<CharacterController>();
+        rb = gameObject.GetComponentInParent<Rigidbody>();
         maximumFuel = fuel;
-	}
+        particle = gameObject.GetComponentInChildren<ParticleSystem>();
+        if (particle == null)
+        {
+            Debug.Log("CANNOT FIND JETPACK PARTICLES.");
+        }
+    }
 
     void Update ()
     {
-        // 플레이어 애니메이션 관리
-        // Set the animation to walk, jump or idle.
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) // Go Foward
-        {
-            anim.SetInteger("WalkParam", 1);
-        }
-        else if (Input.GetKey(KeyCode.S)) // Go Backward
-        {
-            anim.SetInteger("WalkParam", 2);
-        }
-        else // Idle
-        {
-            anim.SetInteger("WalkParam", 0);
-            anim.SetInteger("JumpParam", 0);
-        }
-
-        if (controller.isGrounded)  // 지상
-        {
-            verticalVelocity = -gravity * Time.deltaTime;
-            if(Input.GetKeyDown(KeyCode.Space)) // 점프
-            {
-                verticalVelocity = jumpForce;
-                anim.SetInteger("JumpParam", 1);
-            }
-            anim.SetBool("UsingJetpack", false);
-        }
-        else // 낙하
-        {
-            if(Input.GetKey(KeyCode.Space) && fuel > 0.0f)
-            {
-                isJetpackOn = true;
-                UsingJetpack();
-                anim.SetBool("UsingJetpack", true);
-                fuel -= Time.deltaTime;
-            }
-            else
-            {
-                isJetpackOn = false;
-                verticalVelocity -= gravity * Time.deltaTime;
-
-                // 연료 회복
-                if(fuel < maximumFuel)
-                {
-                    fuel += Time.deltaTime * 0.5f;
-                }
-            }
-        }
-
-        Vector3 moveVector = Vector3.zero;
-        moveVector.x = Input.GetAxis("Horizontal") * 5.0f;
-        moveVector.y = verticalVelocity;
-        moveVector.z = Input.GetAxis("Vertical") * 5.0f;
-        controller.Move(moveVector * Time.deltaTime);
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            anim.SetInteger("JumpParam", 0);
-            isJetpackOn = false;
-            verticalVelocity = -gravity * Time.deltaTime;
-        }
-        else
-        {
-            verticalVelocity -= gravity * Time.deltaTime;
-        }
-
-        return;
-    }
-
-    private void UsingJetpack()
-    {
-        Vector3 jetpackOn = new Vector3(0.0f, verticalVelocity * 0.5f, 0.0f);
-        controller.Move(jetpackOn * Time.deltaTime);
-
-        return;
+        MoveCharacter();
     }
 
     private void UpgradeFuelLimits()
     {
         maximumFuel += 5.0f;
+        return;
+    }
+
+    private void MoveCharacter()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+
+        // 점프 처리
+        if (isGrounded)  // 점프중이 아님
+        {
+            // 에니매이션 처리
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) // Walk foward, left, right.
+                anim.SetInteger("WalkParam", 1);
+            else if (Input.GetKey(KeyCode.S)) // Walk backward
+                anim.SetInteger("WalkParam", 2);
+            else // Idle
+                anim.SetInteger("WalkParam", 0);
+
+            anim.SetInteger("JumpParam", 0);
+            anim.SetBool("UsingJetpack", false);
+
+            if (Input.GetKeyDown(KeyCode.Space)) // 점프
+            {
+                isGrounded = false;
+                anim.SetInteger("JumpParam", 1);
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+        else // 점프 또는 낙하 중
+        {
+            anim.SetInteger("WalkParam", 0);
+            // 연료가 남아있으면 제트팩 사용
+            if (Input.GetKey(KeyCode.Space) && fuel > 0.0f)
+            {
+                isJetpackOn = true;
+            }
+            else
+            {
+                isJetpackOn = false;
+            }
+        }
+        // 제트팩 사용 처리
+        UsingJetpack();
+
+        // 이동 처리
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cam.transform.forward.x, 0f, cam.transform.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cam.transform.right.x, 0f, cam.transform.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            this.transform.forward = lookForward;
+            this.transform.position += moveDir * Time.deltaTime * 5f;
+        }
+    }
+
+    private void UsingJetpack()
+    {
+        switch (isJetpackOn)
+        {
+            case true:
+                anim.SetBool("UsingJetpack", true);
+                particle.Play();
+                fuel -= Time.deltaTime;
+                rb.AddForce(Vector3.up * jumpForce * 2, ForceMode.Force);
+                break;
+            case false:
+                anim.SetBool("UsingJetpack", false);
+                particle.Stop();
+                // 연료 회복
+                if (fuel < maximumFuel)
+                {
+                    fuel += Time.deltaTime * 0.1f;
+                    if (fuel > maximumFuel)
+                        fuel = maximumFuel;
+                }
+                
+                break;
+        }
+        
+        return;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            anim.SetInteger("JumpParam", 0);    // 점프 에니메이션 OFF
+            anim.SetBool("UsingJetpack", false);    // 제트팩 에니메이션 OFF
+            isJetpackOn = false;                // 제트팩 사용 중 아님
+            isGrounded = true;
+        }
+
         return;
     }
 }
--- [PP(2)]Hell's_DIEter/Assets/Scripts/ThirdPersonCamera.cs
+++ [PP(2)]Hell's_DIEter/Assets/Scripts/ThirdPersonCamera.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class ThirdPersonCamera : MonoBehaviour
 {
     private GameObject player;
-    private GameObject mainCamera;
 
-    private float rotSpeed = 50.0f;
+    private float rotSpeed = 2.0f;
 
     private float camDistance = 0;      // 리그부터 카메라까지의 거리
     private float camWidth = -10.0f;    // 가로 거리
@@ -15,54 +14,32 @@ public class ThirdPersonCamera : MonoBehaviour
     private float camFix = 3.0f;        // 레이캐스트 후 리그를 향해 올 보정 값(거리)
 
     Vector3 direction;
+    Vector3 mouseMove;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
 
-        // Length from camera rig to camera.
+        // 카메라 리그부터 카메라까지의 거리
         camDistance = Mathf.Sqrt(camWidth * camWidth + camHeight * camHeight);
 
-        // Direction vector from camera rig to camera position.
+        // 카메라 리그부터 카메라 위치까지의 방향벡터
         direction = new Vector3(0, camHeight, camWidth).normalized;
     }
 
     private void Update()
     {
-        // Rotation from y axsis.
-        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed, Space.World);
-        // Rotation from x axsis.
-        transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * Time.deltaTime * rotSpeed, Space.Self);
+        LookAround();
+    }
 
+    private void LookAround()
+    {
+        // 카메라 회전 관련
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed, Space.World);
+        transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * Time.deltaTime * rotSpeed, Space.Self);
         transform.position = player.transform.position;
 
-        // Vector value for ray casting
-        Vector3 rayTarget = transform.up * camHeight + transform.forward * camWidth;
-
-        RaycastHit hitInfo;
-        Physics.Raycast(transform.position, rayTarget, out hitInfo, camDistance);
-
-        // If ray casting was successful
-        if (hitInfo.point != Vector3.zero)
-        {
-            // Move camera to point.
-            mainCamera.transform.position = hitInfo.point;
-
-            // Fix the camera.
-            mainCamera.transform.Translate(direction * -1 * camFix);
-        }
-        else
-        {
-            // Set the local position to zero. (Move the camera rig.)
-            mainCamera.transform.localPosition = Vector3.zero;
-            // Translate the camera.
-            mainCamera.transform.Translate(direction * camDistance);
-
-            // Fix the camera.
-            mainCamera.transform.Translate(direction * -1 * camFix);
-        }
-
         transform.Translate(player.transform.position);
+        Debug.DrawRay(transform.position, new Vector3(transform.forward.x, 0f, transform.forward.z).normalized, Color.red);
     }
 }
